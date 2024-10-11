c69c3e0ec6c45f363be04c6635b9ad9d683b9121
charlottenguyen05 <nhinguyenngoclinh2005@gmail.com>
2024-10-11 23:52:49 +0200

    fix bug

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,3 +1,12 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = handleAuth();
+export const GET = async (req: NextRequest) => {
+  handleAuth(req);
+  const response = new NextResponse();
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  response.headers.set("Access-Control-Allow-Credentials", "true");
+  return response;
+};
